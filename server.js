7b9d46b1b371f4f331e355f7f7b5014e7037bcc8
Mona Lisa <joannagrosshans2@gmail.com>
2020-11-11 09:51:23 -0600

    server set up

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+//connect css and js files-middleware function
+app.use(express.static('public'));
+//prase incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
