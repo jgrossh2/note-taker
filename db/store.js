b91c96ddb9d46ca928d69dec453088628f4c74d9
Mona Lisa <joannagrosshans2@gmail.com>
2020-11-12 19:55:35 -0600

    router post

--- db/store.js
+++ db/store.js
@@ -1,6 +1,38 @@
-//read db 
-//write db
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-getNote
-addNote--include write to File
-deleteNote
+class Store {
+    read() {
+        return fs.readFile("db/db.json", (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        });
+    }
+    write(note) {
+        return fs.writeFile("db/db.json", JSON.stringify(note));
+    }
+    getNotes(notes) {
+        return this.read().then(notes)
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        // if (!title || !text) {
+        //     return alert("Please include both title and text information.")
+        // }
+        const userId = uuidv4();
+        //adds id to new note object
+        const newNote = { title, text, id: userId };
+        //get notes, add to notes, update notes
+        return this.getNotes()
+            .then(notes.push(newNote))
+            .then(getNotes())
+            .then(write());
+    }
+    // deleteNote
+
+
+
+}
+module.exports = new Store;
--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,33 +1,40 @@
-const { truncateSync } = require('fs');
-const path = require('path');
-// const { report } = require('./htmlRoutes');
 const router = require('express').Router();
 const { notes } = require('../../db/db');
-const {v4 : uuidv4} = require('uuid') 
-// const { saveNote } = require('../../public/assets/js/index');
+const store = require('../../db/store');
 
 router.get('/notes', (req, res) => {
-    //taking notes array and setting as note
-    res.json(note)
+    store
+        .getNotes(notes)
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => {
+            res.json("error");
+        });
+    //taking notes array and setting as note 
 });
 router.post('/notes', (req, res) => {
-    //needing req.body.id
+    store   
+        .addNote(req.body)
+        .then((newNote) => res.json(newNote));
+    // req.body.id= notes.length.toString();
+    
     // const newNote = saveNote(req.body, notes)
-    const newNote = req.body;
-    console.log(newNote)
-    //create an array to push newNote to from db.json file
-    notes.push(newNote);
-    //return new note to client
-    res.json(newNote);
-    //creates id per note
-    const userId = uuidv4()
-    // //saving record to the database 
-    // //with attaching userid to each record
-    const keepNote = newNote.userId 
-    notes.push(keepNote);
-    // res.json(notes)
-    // res.send("New task");
-    console.log(userId)
-})
+    // const newNote = req.body;
+    // console.log(newNote)
+    // //create an array to push newNote to from db.json file
+    // notes.push(newNote);
+    // //return new note to client
+    // res.json(newNote);
+    // //creates id per note
+    // const userId = uuidv4()
+    // // //saving record to the database 
+    // // //with attaching userid to each record
+    // const keepNote = newNote.userId 
+    // notes.push(keepNote);
+    // // res.json(notes)
+    // // res.send("New task");
+    // console.log(userId)
+});
 
 module.exports = router;
