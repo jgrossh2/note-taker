ccad87a9301f8974c5a189c9718151bb7f18812d
Mona Lisa <joannagrosshans2@gmail.com>
2020-11-14 10:15:28 -0600

    route fixed to index

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 });
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
